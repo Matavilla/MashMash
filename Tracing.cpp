f46e298d5e1ada674b33668caeb1a395f5e4dc95
Matavilla <mr.andrej1102@yandex.ru>
2020-03-16 17:05:05 +0300

    dwqrwq

--- Tracing.cpp
+++ Tracing.cpp
@@ -4,23 +4,23 @@ Coord Scene::ToRealCoord(const double& x, const double& y) {
     return Coord((x - w / 2) / (double) (w / 2), (y - h / 2) / (double) (h / 2), 1);
 }
 
-Coord ReflectRay(const Coord R, const Coord& N) {
+inline Coord ReflectRay(const Coord& R, const Coord& N) {
     return (N * (2 * (N * R))) - R;
 }
 
-bool Scene::ClosestIntersection(Sphere& tmp_sp, double& tmp, Coord O, Coord D, double t_min, double t_max) {
+bool Scene::ClosestIntersection(ObjINT** tmp_sp, double& tmp, Coord O, Coord D, double t_min, double t_max) {
     tmp = inf;
     bool flag = false;
     for (auto &sph : sp) {
-        auto i = sph.RayIntersect(O, D);
+        auto i = sph->RayIntersect(O, D);
         if (i.first < tmp && t_min < i.first && t_max > i.first) {
             tmp = i.first;
             flag = true;
-            tmp_sp = sph;
+            *tmp_sp = sph.get();
         }
         if (i.second < tmp && t_min < i.second && t_max > i.second) {
             tmp = i.second;
-            tmp_sp = sph;
+            *tmp_sp = sph.get();
             flag = true;
         }
     }
@@ -28,24 +28,25 @@ bool Scene::ClosestIntersection(Sphere& tmp_sp, double& tmp, Coord O, Coord D, d
 }
 
 Pixel Scene::TraceRay(Coord O, Coord D, double t_min, double t_max, int depth) {
-    Sphere tmp_sp;
+    ObjINT* tmp_sp;
+
     double tmp;
-    
-    if (!ClosestIntersection(tmp_sp, tmp, O, D, t_min, t_max)) {
-        return BG;
-    }
-        
-    Coord P = O + D * tmp;
-    Coord N = tmp_sp.GetNormal(P);
-    Pixel local_color = tmp_sp.color * Lighting(P, N, -D, tmp_sp.specular);
+     
+    if (ClosestIntersection(&tmp_sp, tmp, O, D, t_min, t_max)) {
+        Coord P = O + D * tmp;
+        Coord N = tmp_sp->GetNormal(P);
+        Pixel local_color = tmp_sp->color * Lighting(P, N, -D, tmp_sp->specular);
 
-    if (depth <= 0 || tmp_sp.reflective <= 0)
-        return local_color;
+        if (depth <= 0 || tmp_sp->reflective <= 0)
+            return local_color;
 
-    Coord R = ReflectRay(-D, N);
-    Pixel reflected_color = TraceRay(P, R, 0.001, inf, depth - 1);
+        Coord R = ReflectRay(-D, N);
+        Pixel reflected_color = TraceRay(P, R, 0.001, inf, depth - 1);
     
-    return local_color * (1. - tmp_sp.reflective) + reflected_color * tmp_sp.reflective;
+        return local_color * (1. - tmp_sp->reflective) + reflected_color * tmp_sp->reflective;
+    } 
+
+    return BG;
 }
 
 double Scene::Lighting(Coord P, Coord N, Coord V, double s) {
@@ -61,10 +62,10 @@ double Scene::Lighting(Coord P, Coord N, Coord V, double s) {
                 tmp = 1.;
             }
 
-            Sphere tmp_sp;
+            ObjINT* tmp_sp;
             double dist;
 
-            if (ClosestIntersection(tmp_sp, dist, P, L, 0.001, tmp)) {
+            if (ClosestIntersection(&tmp_sp, dist, P, L, 0.001, tmp)) {
                 continue;
             }
 
--- Tracing.h
+++ Tracing.h
@@ -4,6 +4,7 @@
 #include <limits>
 #include <cmath>
 #include <iostream>
+#include <memory>
 #include <ctime>
 
 #include "Bitmap.h"
@@ -47,26 +48,41 @@ struct Coord{
         return (*this) / len();
     }
 
+    Coord vect(const Coord& b) const {
+        return Coord(y * b.z - z * b.y, z * b.x - x * b.z, x * b.y - y * b.x);
+    }
+
 };
 
-struct Sphere {
-    Coord c;
-    double r;
+struct ObjINT {
     Pixel color;
     int specular;
     double reflective;
 
-    Sphere(Coord _c = Coord(), double r1 = 0., Pixel _col = Pixel(), int _s = 0, double _ref = 0) : c(_c),
-                                                                                                    r(r1),
-                                                                                                    color(_col),
-                                                                                                    specular(_s),
-                                                                                                    reflective(_ref) {}
-    
+    virtual std::pair<double, double> RayIntersect(Coord O, Coord D) = 0;
+
+    virtual Coord GetNormal(const Coord& p) const = 0;
+
+    virtual ~ObjINT() {};
+};
+
+struct Sphere : ObjINT {
+    Coord c;
+    double r;
+
+    Sphere(Coord _c = Coord(), double r1 = 0., Pixel _col = Pixel(), int _s = 0, double _ref = 0) {
+        c = _c;
+        r = r1;
+        color = _col;
+        specular = _s;
+        reflective = _ref;
+    }
+
     Coord GetNormal(const Coord& p) const {
         return (p - c) / r;
     }
 
-    std::pair<double, double> RayIntersect(Coord O, Coord D) {
+    virtual std::pair<double, double> RayIntersect(Coord O, Coord D) override {
         Coord oc = O - c;
 
         double k1 = D * D;
@@ -82,6 +98,49 @@ struct Sphere {
         double t2 = (-k2 - sqrt(disc)) / (2 * k1);
         
         return std::make_pair(t1, t2);
+    }
+
+};
+
+struct Triangle : ObjINT {
+    Coord a, b, c;
+
+    Triangle(const Coord& _a, const Coord& _b, const Coord& _c, const Pixel& _col, int _s, double _ref) {
+        a = _a;
+        b = _b;
+        c = _c;
+        color = _col;
+        specular = _s;
+        reflective = _ref;
+    }
+
+    std::pair<double, double> RayIntersect(Coord O, Coord D) override {
+        Coord e1 = b - a;
+        Coord e2 = c - a;
+        Coord pvec = D.vect(e2);
+        
+        double disc = e1 * pvec;
+        if (disc < 0) {
+            return std::make_pair(inf, inf);
+        }
+
+        Coord tvec = O - a;
+        double u = tvec * pvec;
+        if (u < 0 || u > disc) {
+            return std::make_pair(inf, inf);
+        }
+
+        Coord qvec = tvec.vect(e1);
+        double v = D * qvec;
+        if (v < 0 || u + v > disc) {
+            return std::make_pair(inf, inf);
+        }
+
+        return std::make_pair((e2 * qvec) / disc, inf);
+    }
+
+    Coord GetNormal(const Coord& c) const {
+        return c.vect(b - a);
     }
 };
 
@@ -101,25 +160,31 @@ struct Light {
 };
 
 struct Scene {
-    std::vector<Sphere> sp;
+    std::vector<std::unique_ptr<ObjINT>> sp;
     std::vector<Light> li;
 
     Pixel BG;
-    int w = 800;
-    int h = 800;
+    int w = 700;
+    int h = 700;
 
     Scene() {
         BG  = Pixel(0, 0, 0);
 
-        sp.push_back(Sphere(Coord(2, 0, 4), 1, Pixel(133, 21, 199), 500, 0.3));
-        sp.push_back(Sphere(Coord(0, -1, 3), 1, Pixel(238, 104, 123), 50, 0));
-        sp.push_back(Sphere(Coord(-2, 0, 4), 1, Pixel(47, 255, 173), 100, 0.9));
-        sp.push_back(Sphere(Coord(0, -5001, 0), 5000, Pixel(192, 192, 192), 1000, 0.05));
-        sp.push_back(Sphere(Coord(130, 140, 180), 12, Pixel(225, 228, 255), 2000, 0.04));
+        sp.emplace_back(new Sphere(Coord(2, 0, 5), 1, Pixel(133, 21, 199), 500, 0.3));
+        sp.emplace_back(new Sphere(Coord(0, -1, 4), 1, Pixel(238, 104, 123), 50, 0));
+        sp.emplace_back(new Sphere(Coord(-2, 0, 5), 1, Pixel(47, 255, 173), 100, 0.9));
+        sp.emplace_back(new Sphere(Coord(0, -5001, 0), 5000, Pixel(192, 192, 192), 1000, 0.05));
+        sp.emplace_back(new Sphere(Coord(130, 140, 180), 12, Pixel(225, 228, 255), 2000, 0.04));
 
-        li.push_back(Light(Light::AMBIENT, 0.1));
-        li.push_back(Light(Light::POINT, 0.8, Coord(2, 1, 0)));
-        li.push_back(Light(Light::DIRECTIONAL, 0.1, Coord(1, 4, 4)));
+
+        sp.emplace_back(new Triangle(Coord(2, 0.5, 3), Coord(0, 2, 2.5), Coord(-2, 0.5, 3), Pixel(255, 255, 12), 100, 0.02));
+        sp.emplace_back(new Triangle(Coord(0, 0, 2), Coord(2, 0.5, 3), Coord(-2, 0.5, 3), Pixel(255, 45, 123), 100, 0.02));
+        sp.emplace_back(new Triangle(Coord(0, 0, 2), Coord(-2, 0.5, 3), Coord(0, 2, 2.5), Pixel(9201, 131, 255), 100, 0.02));
+        sp.emplace_back(new Triangle(Coord(2, 0.5, 3), Coord(0, 0, 2), Coord(0, 2, 2.5), Pixel(56, 123, 12), 100, 0.02));
+
+        li.push_back(Light(Light::AMBIENT, 0.2));
+        li.push_back(Light(Light::POINT, 0.6, Coord(2, 1, 0)));
+        li.push_back(Light(Light::DIRECTIONAL, 0.2, Coord(1, 4, 4)));
     }
     Coord ToRealCoord(const double& x, const double& y);
 
@@ -127,6 +192,6 @@ struct Scene {
 
     double Lighting(Coord p, Coord n, Coord v, double s);
 
-    bool ClosestIntersection(Sphere& tmp_sp, double& tmp, Coord O, Coord D, double t_min = 1, double t_max = inf);
+    bool ClosestIntersection(ObjINT** tmp_sp, double& tmp, Coord O, Coord D, double t_min = 1, double t_max = inf);
 };
 
